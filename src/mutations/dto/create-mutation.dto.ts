73505cc38709420ca7050f58cbfc6d99615d568c
Samuel Zuniga Vanoye <samuelzv@gmail.com>
2019-11-21 21:47:47 -0600

    Define data transfer object to receive dna payloads

--- src/mutations/dto/create-mutation.dto.ts
+++ src/mutations/dto/create-mutation.dto.ts
@@ -1,7 +1,15 @@
 /**
  * Data Transfer Object used by creating mutations
  */
+import { ApiModelProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsArray } from 'class-validator';
 
 export class CreateMutationDto {
+    @ApiModelProperty({
+        description: 'Array of strings containing dna sequences',
+        example: ["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"]
+    })
+    @IsNotEmpty()
+    @IsArray()
     dna: string[];
 }

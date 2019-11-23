459b529ebb5f5939bc89f40723e236778f7c0715
Samuel Zuniga Vanoye <samuelzv@gmail.com>
2019-11-23 08:45:03 -0600

    Isolate database configuration into a constant

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,12 +4,12 @@ import * as config from 'config';
 const dbConfig = config.has('db') ? config.get('db') : {};
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: process.env.RDS_TYPE || dbConfig.type,
-    host: process.env.RDS_HOSTNAME || dbConfig.host,
-    port: process.env.RDS_PORT || dbConfig.port,
-    database: process.env.RDS_DB_NAME || dbConfig.database,
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
+    type: process.env.ADN_RDS_TYPE || dbConfig.type,
+    host: process.env.ADN_RDS_HOSTNAME || dbConfig.host,
+    port: process.env.ADN_RDS_PORT || dbConfig.port,
+    database: process.env.ADN_RDS_DB_NAME || dbConfig.database,
+    username: process.env.ADN_RDS_USERNAME,
+    password: process.env.ADN_RDS_PASSWORD,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
 };

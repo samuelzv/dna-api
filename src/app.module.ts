ade1f3a022a57cda016a40a7ca0927b747ac1f76
Samuel Zuniga Vanoye <samuelzv@gmail.com>
2019-11-24 17:26:51 -0600

    On root path hit redirect to api path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import {Module} from '@nestjs/common';
 import {MutationsModule} from './mutations/mutations.module';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {typeOrmConfig} from './config/typeorm.config';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
       TypeOrmModule.forRoot(typeOrmConfig),
       MutationsModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
